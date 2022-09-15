33cedc94f2d6fcfa184589e5639b55e5ff85871b
Md. Shahadot Hossain <shahadotice@gmail.com>
2022-09-15 18:04:42 +0600

    ok

--- CompanyEmployees/CompanyEmployees/Controllers/WeatherForecastController.cs
+++ CompanyEmployees/CompanyEmployees/Controllers/WeatherForecastController.cs
@@ -12,10 +12,10 @@ namespace CompanyEmployees.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        //private static readonly string[] Summaries = new[]
-        //{
-        //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        //};
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private ILoggerManager _logger;
         public WeatherForecastController(ILoggerManager logger)
@@ -23,28 +23,32 @@ namespace CompanyEmployees.Controllers
             _logger = logger;
         }
 
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    _logger.LogInfo("Here is info message from our values controller.");
+        //    _logger.LogDebug("Here is debug message from our values controller.");
+        //    _logger.LogWarn("Here is warn message from our values controller.");
+        //    _logger.LogError("Here is an error message from our values controller.");
+        //    return new string[] { "value1", "value2" };
+        //}
+
+
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<WeatherForecast> Get()
         {
             _logger.LogInfo("Here is info message from our values controller.");
             _logger.LogDebug("Here is debug message from our values controller.");
             _logger.LogWarn("Here is warn message from our values controller.");
             _logger.LogError("Here is an error message from our values controller.");
-            return new string[] { "value1", "value2" };
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
         }
-
-
-        //[HttpGet]
-        //public IEnumerable<WeatherForecast> Get()
-        //{
-        //    var rng = new Random();
-        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-        //    {
-        //        Date = DateTime.Now.AddDays(index),
-        //        TemperatureC = rng.Next(-20, 55),
-        //        Summary = Summaries[rng.Next(Summaries.Length)]
-        //    })
-        //    .ToArray();
-        //}
     }
 }
